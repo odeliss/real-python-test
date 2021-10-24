cbdc044e3f84717fc0df0f535773de8b95a66453
Olivier De Lissnyder <olivier@wapetex.com>
2021-10-24 10:58:45 -0400

    Added routes with values

--- app.py
+++ app.py
@@ -11,10 +11,31 @@ app= Flask(__name__)
 @app.route("/hello")
 
 #define the view using a function which returns a string
+#this function is called by both / and by /hello
 def hello_world():
-	return("Hello World")
+	#the controller will render this simple text on the screen
+	return("Hello World") 
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return (search_query)
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print (value+1)   #the value is printed on the terminal 
+	return ("Correct") #the text is returned to the controller for html-ization
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print (value+1)
+	return ("Correct")
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print (value)
+	return ("Correct")
+
 
 #start the dev server using the run() method
 if __name__ == "__main__":
 	app.run()
-	

6fcae6c3dd4284d1cc321f48b08424c09b3f7d5c
Olivier De Lissnyder <olivier@wapetex.com>
2021-10-25 03:09:12 -0400

    Added debugger and a few more routes

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask
 #create the app object
 app= Flask(__name__)
 
+#configure the app object 
+app.config["DEBUG"] = True #Debug on console + reloader each time a code change is made
+
 #use decorator to link a function to a URL
 @app.route("/")
 @app.route("/hello")
@@ -13,12 +16,17 @@ app= Flask(__name__)
 #define the view using a function which returns a string
 #this function is called by both / and by /hello
 def hello_world():
-	#the controller will render this simple text on the screen
-	return("Hello World") 
+	'''
+	the controller will render this simple text on the screen
+	If not specified, Flask assings a return code 200
+	and a Content-Type "text/html"
+
+	'''
+	return("Hello World, with an automated restart")
 
 @app.route("/test/<search_query>")
 def search(search_query):
-	return (search_query)
+	return (search_query) 
 
 @app.route("/integer/<int:value>")
 def int_type(value):
@@ -35,6 +43,12 @@ def path_type(value):
 	print (value)
 	return ("Correct")
 
+@app.route("/name/<name>")
+def index(name):
+	if name.lower()=="olivier" :
+		return ("Hello,{}".format(name))
+	else :
+		return ("This name is not found", 404)
 
 #start the dev server using the run() method
 if __name__ == "__main__":
